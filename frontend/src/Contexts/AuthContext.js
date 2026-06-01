aa0b9455a505d5701a1bd2c19cb1481604e57793
sabrina <sabrina130102@gmail.com>
2026-06-01 14:01:06 +0200

    added export useAuth

--- frontend/src/Contexts/AuthContext.js
+++
@@ -1,8 +0,0 @@
-// Based on code from Nora Storro (Fullstack assignment 3)
-import { createContext } from "react";
-
-export const AuthContext = createContext({
-  user: null,
-  login: () => {},
-  logout: () => {},
-});
